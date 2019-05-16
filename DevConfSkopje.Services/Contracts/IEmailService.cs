a5949df1e7ac8a99b2f41c3a11a71047e8a505ba
a4katabg <anangelov@melontech.com>
2019-05-16 11:56:36 +0300

    Add send feedback emails functionality

--- DevConfSkopje.Services/Contracts/IEmailService.cs
+++ DevConfSkopje.Services/Contracts/IEmailService.cs
@@ -1,7 +1,10 @@
-﻿namespace DevConfSkopje.Services.Contracts
+﻿using System.Collections.Generic;
+
+namespace DevConfSkopje.Services.Contracts
 {
     public interface IEmailService
     {
         void SendCorfimation(string emailTo, string pathToEmailTemplate);
+        void SendFeedbackMessages(List<string> emails, string pathToEmailTemplate);
     }
 }
